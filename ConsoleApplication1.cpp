ff909912351c681576ee3f445af498ed0d4aebc2
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-07 06:41:37 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+int  main() {
+	int n, i = 0;
+	scanf("%d", &n);
+	printf("\n\n\n你输入的数字是%d\n ", n);
+	while (n != 1) {
+		if (n % 2 == 0) {
+			n = n / 2;
+			++i;
+		}									//如果n是偶数
+		else if (n % 2 == 1) {
+			n = (3 * n + 1) / 2;
+			++i;
+		}									//如果n是奇数
+	}
+	printf("\n\n\n需要执行的次数是%d", i);
+	return 0;
+}
