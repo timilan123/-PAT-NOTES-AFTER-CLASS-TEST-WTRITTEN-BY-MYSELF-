02899c8fe5daf0a9946dcf2cd5c5a07d671062d1
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-07 06:49:25 +0800

    Update 害死人不偿命的(3n+1)猜想.cpp

--- 害死人不偿命的(3n+1)猜想.cpp
+++ 害死人不偿命的(3n+1)猜想.cpp
@@ -1,17 +1,17 @@
 ﻿#include<stdio.h>
 int  main() {
 	int n, i = 0;                             //注意了，像这样的要统计计算次数的代码，i初始的时候都为0.
-	scanf("%d", &n);
+	scanf("%d", &n);                          //注意记忆scanf和printf括号里面%和&的写法
 	printf("%d\n ", n);
-	while (n != 1) {
+	while (n != 1) {                          //如果n是偶数（假如这里的括号里面只写了一个n，它代表的意思就是n!=0，即n不为NULL，或者说n存在）
 		if (n % 2 == 0) {
 			n = n / 2;
 			++i;
-		}									//如果n是偶数
-		else if (n % 2 == 1) {
+		}									
+		else if (n % 2 == 1) {            //如果n是奇数（注意这里是else if，新手不要漏掉else）
 			n = (3 * n + 1) / 2;
 			++i;
-		}									//如果n是奇数
+		}									
 	}
 	printf("%d", i);
 	return 0;
