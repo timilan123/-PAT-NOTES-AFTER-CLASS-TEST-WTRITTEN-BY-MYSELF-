4f79c3cb5e414c4930a878015a5e3d68e7927795
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-12 18:49:55 +0800

    Update 问题 E: 例题3-5 求一元二次方程的根.cpp

--- 问题 E: 例题3-5 求一元二次方程的根.cpp
+++ 问题 E: 例题3-5 求一元二次方程的根.cpp
@@ -5,6 +5,6 @@ int main() {
 	scanf("%lf%lf%lf", &a, &b, &c);
 	double r1 = (-b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
 	double r2 = (-b - sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
-	printf("r1=%7.2f\nr2=%7.2f\n", r1,r2);
+	printf("r1=%7.2f\nr2=%7.2f\n", r1,r2);                   //别忘记打r1=和r2=了，7表示输出的数字包括小数点包括小数共7位，.2代表该小数点后面保留2位小数
 	return 0;
 }                                              
