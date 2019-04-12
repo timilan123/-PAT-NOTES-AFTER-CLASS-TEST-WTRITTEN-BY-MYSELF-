306d6b6e519fa3950a885d27849bd74ce4fdd4ad
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-12 18:47:00 +0800

    Update and rename ConsoleApplication1.cpp to 问题 E: 例题3-5 求一元二次方程的根.cpp

--- 问题 E: 例题3-5 求一元二次方程的根.cpp
+++ 问题 E: 例题3-5 求一元二次方程的根.cpp
@@ -5,6 +5,6 @@ int main() {
 	scanf("%lf%lf%lf", &a, &b, &c);
 	double r1 = (-b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
 	double r2 = (-b - sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
-	printf("%7.2f\n%7.2f\n", r1,r2);
+	printf("%7.2f\n%7.2f\n", r1,r2);                //7代表输出的数字包括小数包括标点符号占7位，.2代表小数保留2位
 	return 0;
 }                                              
