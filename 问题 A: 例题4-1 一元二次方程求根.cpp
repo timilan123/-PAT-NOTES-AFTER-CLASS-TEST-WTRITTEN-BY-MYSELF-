66d0c7d59e53d2de1aaa0f0da748f487863e7ba4
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-13 19:25:16 +0800

    Update 问题 A: 例题4-1 一元二次方程求根.cpp

--- 问题 A: 例题4-1 一元二次方程求根.cpp
+++ 问题 A: 例题4-1 一元二次方程求根.cpp
@@ -6,7 +6,7 @@ int main() {
 	if (pow(b, 2) - 4 * a * c < 0) {
 		printf("No real roots!\n");
 	}
-	else if (pow(b, 2) - 4 * a * c >= 0) {
+	else if (pow(b, 2) - 4 * a * c >= 0) {                                   //也可以用else
 		double r1 = (-b + sqrt((pow(b, 2)) - 4 * a * c)) / (2 * a);
 		double r2 = (-b - sqrt((pow(b, 2)) - 4 * a * c)) / (2 * a);
 		printf("r1=%7.2f\nr2=%7.2f\n", r1, r2);
