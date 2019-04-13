2e0a3426bb898bb82a2c0b2c91906b0646d61626
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-13 20:02:55 +0800

    Update and rename ConsoleApplication1.cpp to 问题 C: 例题4-3 比较交换3个实数值，并按序输出.cpp

--- 问题 C: 例题4-3 比较交换3个实数值，并按序输出.cpp
+++ 问题 C: 例题4-3 比较交换3个实数值，并按序输出.cpp
@@ -3,7 +3,7 @@ int main() {
 	double a, b, c,i;
 	scanf("%lf%lf%lf", &a, &b, &c);
 	if (a > b) {
-		i = a; a = b; b = i;
+		i = a; a = b; b = i;               //交换3次即可
 }
 	if (a > c) {
 		i = a; a = c; c = i;
