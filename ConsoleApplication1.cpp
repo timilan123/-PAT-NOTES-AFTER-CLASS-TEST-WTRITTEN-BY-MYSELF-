ba7bf545a39cc8f1b5336160fd531c69a9b13286
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-13 20:07:25 +0800

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,7 +1,7 @@
 ﻿#include<stdio.h>
 int main() {
 	int a, b, c;
-	scanf("%d%d%d", &a, &b, &c);
+	scanf("%d%d%d", &a, &b, &c);             /*把a、b、c逐个与另外两个数进行对比即可,这里会用到“且”的符号"&&"*/
 	if (a > b && a > c)
 		printf("%d", a);
 	else if (b > a && b > c)
