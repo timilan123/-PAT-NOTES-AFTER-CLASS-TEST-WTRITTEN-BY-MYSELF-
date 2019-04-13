a8a5baad689a1677a07478c102b1010e1c3d74d2
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-13 23:21:43 +0800

    Update and rename ConsoleApplication1.cpp to 【有点难】算圆周率.cpp

---
+++ 【有点难】算圆周率.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+#include<math.h>
+int main( ) {
+	int sign = 1;
+	double PI = 0.0, n = 1, term = 1.0;      //分母可以不用加小数点
+	while (fabs(term) >= 0.000001) {                 
+		PI = PI + term;
+		n = n + 2;
+		sign = -sign;                     //少用pow()这样的函数，多使用加减乘数这样的简单符号
+		term = sign / n;
+	}
+	PI = PI * 4;
+	printf("PI=%10.8f\n", PI);
+	return 0;
+}
