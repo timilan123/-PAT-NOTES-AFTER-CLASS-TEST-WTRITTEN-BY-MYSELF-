a9bee2372c8cba1c3e882cf707ba814cd7f134ed
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-13 21:14:04 +0800

    Update and rename ConsoleApplication1.cpp to 问题 C: 例题5-1-3 连续自然数求和.cpp

---
+++ 问题 C: 例题5-1-3 连续自然数求和.cpp
@@ -0,0 +1,10 @@
+﻿#include<stdio.h>
+int main() {
+	int m = 0;
+	for (int i = 1; i <= 100;++i)    //这里要用<=100，和while中的101不一样，while里面是到101停止，for这里是执行到100，但是要注意语句结束之后i 的值会变成101
+	{
+		m += i;
+	}
+	printf("%d", m);
+	return 0;
+}
