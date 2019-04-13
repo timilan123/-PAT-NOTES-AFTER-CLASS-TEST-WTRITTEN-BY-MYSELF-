2d3679972e971e17eb0cadfd2bf9a3f65e0d8195
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-13 23:17:55 +0800

    Update and rename ConsoleApplication1.cpp to #include<stdio.h> #include<math.h> int main( ) { 	int sign = 1; 	double PI = 0.0, n = 1, term = 1.0; 	while (fabs(term) >= 0.000001) { 		PI = PI + term; 		n = n + 2; 		sign = -sign; 		term = sign/[易错]例题5-7 求圆周率pi的近似值.cpp

---
+++ #include<stdio.h> #include<math.h> int main( ) { 	int sign = 1; 	double PI = 0.0, n = 1, term = 1.0; 	while (fabs(term) >= 0.000001) { 		PI = PI + term; 		n = n + 2; 		sign = -sign; 		term = sign/[易错]例题5-7 求圆周率pi的近似值.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+#include<math.h>
+int main( ) {
+	int sign = 1;
+	double PI = 0.0, n = 1, term = 1.0;    //声明double变量的时候一定要加小数点,被除数n可以不加小数点
+	while (fabs(term) >= 0.000001) {
+		PI = PI + term;
+		n = n + 2;                    //这里面记得用最简单的加减乘除号，如果使用了过多内置的公式如 pow()，就会产生误差，虽然很小但是题目还是会报错
+		sign = -sign;
+		term = sign / n;
+	}
+	PI = PI * 4;
+	printf("PI=%10.8f\n", PI);
+	return 0;
+}
