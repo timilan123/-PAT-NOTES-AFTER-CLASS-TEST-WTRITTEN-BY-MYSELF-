32de8991da173622237e00c242e532e72b88fe7f
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-13 20:02:26 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+int main() {
+	double a, b, c,i;
+	scanf("%lf%lf%lf", &a, &b, &c);
+	if (a > b) {
+		i = a; a = b; b = i;
+}
+	if (a > c) {
+		i = a; a = c; c = i;
+	}
+	if (b > c) {
+		i = b; b = c; c = i;
+	}
+	printf("%.2f %.2f %.2f\n", a, b, c);
+	return 0;
+}                                              
