1d7b4f1fb4a688fd2932278b4611a0a4208dc98b
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-14 21:42:34 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+int main() {
+	double a = 1.0, b = 2.0,num,all=2.0;
+		for (int i = 3; i <= 21; ++i) {
+			
+			num = a + b; 
+			all = all + (num / b);
+			a = b;
+			b = num;
+		}
+		printf("%.6f\n", all);
+	
+	return 0;
+}
