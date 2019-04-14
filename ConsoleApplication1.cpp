647653586718cf321448e2e6c07bfbf3b11c51e8
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-14 20:59:33 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+int main() {
+	int a = 1, b = 1,num;
+	int n;
+	scanf("%d", &n);
+	if (n <= 2)
+		printf("1\n");
+	if (n > 2) {
+		for (int i = 3; i <= n; ++i) {
+			num = a + b; 
+			b = a;
+			a = num;
+		}
+		printf("%d", num);
+	}
+	return 0;
+}
