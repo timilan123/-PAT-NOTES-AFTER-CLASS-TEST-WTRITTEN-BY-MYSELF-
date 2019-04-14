5f14bed7dccf4193411c5c60dec130a8b1629efa
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-14 21:41:09 +0800

    Update and rename ConsoleApplication1.cpp to #include<stdio.h> int main() { 	double a = 1.0, b = 2.0,num,all=2.0; 		for (int i = 3; i <= 21; ++i) { 			num = a + b;  			all = all + (num/问题 I: 习题5-10 分数序列求和.cpp

--- #include<stdio.h> int main() { 	double a = 1.0, b = 2.0,num,all=2.0; 		for (int i = 3; i <= 21; ++i) { 			num = a + b;  			all = all + (num/问题 I: 习题5-10 分数序列求和.cpp
+++ #include<stdio.h> int main() { 	double a = 1.0, b = 2.0,num,all=2.0; 		for (int i = 3; i <= 21; ++i) { 			num = a + b;  			all = all + (num/问题 I: 习题5-10 分数序列求和.cpp
@@ -4,7 +4,7 @@ int main() {
 		for (int i = 3; i <= 21; ++i) {
 			
 			num = a + b; 
-			all = all + (num / b);
+			all = all + (num / b);           //首先要输出一组数，每个数等于前面的两个数字之和，然后在草稿纸上算好重复几次就行
 			a = b;
 			b = num;
 		}
