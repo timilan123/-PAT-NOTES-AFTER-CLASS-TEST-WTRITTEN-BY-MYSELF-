b6e645674af250ab9aa29cd1cfc12bef77167814
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-14 21:00:13 +0800

    Update and rename ConsoleApplication1.cpp to 问题 H: 例题5-8 Fibonacci数列.cpp

--- 问题 H: 例题5-8 Fibonacci数列.cpp
+++ 问题 H: 例题5-8 Fibonacci数列.cpp
@@ -6,7 +6,7 @@ int main() {
 	if (n <= 2)
 		printf("1\n");
 	if (n > 2) {
-		for (int i = 3; i <= n; ++i) {
+		for (int i = 3; i <= n; ++i) {     //可以用数组，但是不知道为什么数组算的总是不对
 			num = a + b; 
 			b = a;
 			a = num;
