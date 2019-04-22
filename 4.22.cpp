858969e8f11a8a108f451b2f00f641539c11d89c
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-22 22:35:54 +0800

    Add files via upload

---
+++ 4.22.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int  main(){
+int a,b,c,x;
+scanf("%d %d %d",&a,&b,&c);
+int *p1=&a,*p2=&b,*p3=&c,*temp=&x;
+if(*p1<*p2){*temp=*p1;*p1=*p2;*p2=*temp;}
+if(*p2<*p3){*temp=*p2;*p2=*p3;*p3=*temp;}
+if(*p1<*p2){*temp=*p1;*p1=*p2;*p2=*temp;}
+printf("%d %d %d",*p1,*p2,*p3);
+return 0;
+}
+
