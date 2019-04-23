ae9d1b26f04a875169a29935c77a9a36442883e9
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-23 23:50:56 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+#include<string.h>
+void swap(char p1[], char p2[])
+{
+	char p[100];
+	strcpy(p, p1);
+	strcpy(p1, p2);
+	strcpy(p2, p);
+}
+
+int main() {
+	char s1[100], s2[100], s3[100];
+	gets_s(s1);
+	gets_s(s2);
+	gets_s(s3);
+	if (strcmp(s1, s3) > 0)
+		swap(s1, s3);
+	if (strcmp(s2, s3) > 0)
+		swap(s2, s3);
+	if (strcmp(s1, s2) > 0)
+		swap(s1, s2);
+	puts(s1);
+	puts(s2);
+	puts(s3);
+	return 0;
+}
