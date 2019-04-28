fa3093da241acd777a04ee721479ac809d341ee7
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-04-28 23:00:02 +0800

    Update 【比较神奇的一题】问题 D: A+B 输入输出练习IV.cpp

--- 【比较神奇的一题】问题 D: A+B 输入输出练习IV.cpp
+++ 【比较神奇的一题】问题 D: A+B 输入输出练习IV.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
 	int N,read,count=0;
-	while(scanf("%d",&N)){
+	while(scanf("%d",&N)){ /*scanf会自动跳过空格 不停地读取空格之间的数字 在遇到换行的时候停止*/
 		if(N==0)
 		break;
 		for(int i=0;i<N;i++){
