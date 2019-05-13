ef6372e0cbc8313741d5a150bbb63c27564d8574
timilan123 <48851313+timilan123@users.noreply.github.com>
2019-05-13 23:33:07 +0800

    Update 用递归求阶乘.cpp

--- 用递归求阶乘.cpp
+++ 用递归求阶乘.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int jc(int n) {
 	if(n==0) return 1;//因为0！=1，所以把边界设置为0，当n变成0的时候停止递归
-	else 	 return n=jc(n-1)*n;
+	else 	 return n=n*jc(n-1);//上面注释里的边界的n指的是函数参数里面的n不是左边式子里面的n，里面的式子的n-1只代表了计算方法
 }
 
 int main() {
